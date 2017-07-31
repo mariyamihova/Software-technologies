135c82e6ed4b35f81d2a3ecda14e7acddb6e55d0
mimihova <mimihova@gmail.com>
2017-07-31 14:33:52 +0300

    Blog basic functionality in JavaScript

---
+++ JS-Blog/controllers/article.js
@@ -0,0 +1,45 @@
+const Article=require('mongoose').model('Article');
+module.exports={
+    createGet:(req,res)=>{
+        res.render('article/create')
+    },
+    createPost:(req,res)=>{
+        let articleParts=req.body;
+
+        let errorMsg='';
+        if(!req.isAuthenticated()){
+            errorMsg="Sorry, you must be logged in!"
+        }else if(!articleParts.title){
+            errorMsg="Title is required!";
+        }else if(!articleParts.content){
+            errorMsg="Content is required!";
+        }
+        if(errorMsg){
+            res.render('article/create',{error:errorMsg});
+            return;
+        }
+
+        let userId=req.user.id;
+        articleParts.author=userId;
+Article.create(articleParts).then(article=>{
+    req.user.articles.push(article.id);
+    req.user.save(err=>{
+        if(err){
+            res.render('article/create',{
+                error:errorMsg});
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+    },
+
+    detailsGet:(req,res)=>{
+        let id=req.params.id;
+Article.findById(id).populate('author').then(article=>{
+    res.render('article/details',article);
+});
+
+    }
+};
