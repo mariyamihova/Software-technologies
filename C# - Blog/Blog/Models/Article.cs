34a13b3b01636c1c3c78886b5357b760d6a965c9
mimihova <mimihova@gmail.com>
2017-08-28 12:12:53 +0300

    Blog in C#

---
+++ C# - Blog/Blog/Models/Article.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    public class Article
+    {
+        public Article()
+        {
+            this.DateAdded = DateTime.Now;
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        public bool IsAuthor(string name)
+        {
+            return this.Author.UserName.Equals(name);
+        }
+    }
+}
