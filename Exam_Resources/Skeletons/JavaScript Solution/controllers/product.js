baab2d6ed2405f2bb81c385c5a2ab15f03634ccd
mimihova <mimihova@gmail.com>
2017-11-24 11:39:17 +0200

    Exam files - Shopping list

---
+++ Exam_Resources/Skeletons/JavaScript Solution/controllers/product.js
@@ -0,0 +1,43 @@
+const Product = require('../models/Product');
+
+module.exports = {
+	index: (req, res) => {
+        Product.find().then(entries => {
+                res.render('product/index', {'entries': entries});
+        });
+    	},
+	createGet: (req, res) => {
+        res.render('product/create');
+	},
+	createPost: (req, res) => {
+        let product = req.body;
+        Product.create(product).then(product => {
+            res.redirect("/");
+        }).catch(err => {
+            res.render('product/create', product);
+        });
+	},
+	editGet: (req, res) => {
+        let productId = req.params.id;
+        Product.findById(productId).then(product => {
+            if (product) {
+                res.render('product/edit', product );
+            }
+            else {
+                res.redirect('/');
+            }
+        }).catch(err => res.redirect('/'));
+	},
+	editPost: (req, res) => {
+        let productId = req.params.id;
+        let product = req.body;
+
+        Product.findByIdAndUpdate(productId, product, {runValidators: true}).then(product => {
+            res.redirect("/");
+        }).catch(err => {
+            product.id = productId;
+            product.error = "Cannot edit product.";
+            return res.render("product/edit", product);
+        });
+	}
+};
